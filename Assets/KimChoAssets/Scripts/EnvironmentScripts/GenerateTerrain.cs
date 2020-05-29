f85f82d0028e9da1f9166861ec386e7c7724946a
kimdav011 <kimdav011@gmail.com>
2020-05-29 01:21:08 -0700

    update

--- Assets/KimChoAssets/Scripts/EnvironmentScripts/GenerateTerrain.cs
+++ Assets/KimChoAssets/Scripts/EnvironmentScripts/GenerateTerrain.cs
@@ -237,7 +237,7 @@ public class GenerateTerrain : MonoBehaviour
     {
         limitValues();
 
-        if (this.transform.position.z <= -30 && this.transform.position.z >= -50)
+        if (this.transform.position.z <= -30 && this.transform.position.z >= -40)
         {
             return;
         }
